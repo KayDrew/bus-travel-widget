b229771bd224dc4ce4f392bd8f30af8755f934e5
KayDrew <kdtshehla@gmail.com>
2023-05-31 11:54:28 +0200

    added style

--- js/bus-travel-cost.dom.js
+++ js/bus-travel-cost.dom.js
@@ -1,7 +1,33 @@
-document.querySelector (" ");
-document.querySelector (" ");
-document.querySelector (" ");
-document.querySelector (" ");
-document.querySelector (" ");
-document.querySelector (" ");
-document.querySelector (" ");
+let enterPoints=document.querySelector ("#points");
+let place=document.querySelector ("#place");
+let radios=document.querySelectorAll("input[name='time'] ");
+let checkbox=document.querySelector ("#check");
+let calculateBtn=document.querySelector ("#calculate");
+let singleTrips=document.querySelector ("#text1");
+let pricePerSingle=document.querySelector ("#text2");
+let returnTrips=document.querySelector ("#text3");
+let pricePerReturn=document.querySelector ("#text4");
+
+let busFare= calculateFare();
+
+function calculate(){
+
+let time="";
+let startPlace=place.value;
+
+for(radio of radios){
+
+if(radio.checked){
+
+time=radio.value;
+}
+
+}
+
+pricePerSingle.innerHTML=time;
+singleTrips.innerHTML=startPlace;
+returnTrips.innerHTML="10";
+pricePerReturn.innerHTML="10";
+}
+
+calculateBtn.addEventListener("click",calculate);
